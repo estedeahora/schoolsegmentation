0e202be595b59dcee9f8dcc6b3d23313d4855933
estedeahora <pabloserrati@gmail.com>
2022-03-28 20:54:08 -0300

    Functions dependeces

--- analysis/scripts/01_fx.R
+++ analysis/scripts/01_fx.R
@@ -11,34 +11,35 @@ wcomment <- function(cm, txt = "", author = "PS", date = Sys.Date(), comN = comm
 # Calcular centroide ponderado
 
 wtc <- function(g = .data, w = NULL, cl = NULL){
-  if (!(is(g,"sf")) | !(w %in% colnames(g))){
-    stop(paste("requires an sf object with at a column",w))
+  if (!(is(g, "sf")) | !(w %in% colnames(g))){
+    stop(paste("requires an sf object with at a column", w))
   }
   names(g)[names(g) == w] <- "w"
-  centers <- st_coordinates(st_centroid(g)) %>% 
-    cbind(g) %>% 
-    st_drop_geometry() %>% 
-    group_by_at(cl) %>% 
-    summarise(X = weighted.mean(X, w),
-              Y = weighted.mean(Y, w)) %>% 
-    as.data.frame() %>% 
-    st_as_sf(coords = c("X", "Y"), crs = st_crs(g))
-  
+  centers <- sf::st_coordinates(sf::st_centroid(g)) %>%
+    cbind(g) %>%
+    sf::st_drop_geometry() %>%
+    dplyr::group_by_at(cl) %>%
+    dplyr::summarise(X = weighted.mean(X, w),
+              Y = weighted.mean(Y, w)) %>%
+    as.data.frame() %>%
+    sf::st_as_sf(coords = c("X", "Y"), crs = sf::st_crs(g))
+
   return(centers)
 }
 
 
 # f_seg ---------------------------------------------------------------------
+# Calculo de Mutual Information Index Total + Between + Within
 
 f_seg <- function(db, VAR, grupo = "group", peso = "peso", id = "ID_s"){
-  res <- data.frame(Global = mutual_total(data = db, group = grupo,
-                                          unit = "ID_s", weight = peso,
-                                          se = F)$est[1],
-                    Between = mutual_total(data = db, group = grupo,
-                                           unit = VAR, weight = peso,
-                                           se = F)$est[1],
-                    Within =  mutual_total(data = db, group = grupo,
-                                           unit = id, weight = peso,
-                                           se = F, within = VAR)$est[1])
+  res <- data.frame(Global = segregation::mutual_total(data = db, group = grupo,
+                                                       unit = "ID_s", weight = peso,
+                                                       se = F)$est[1],
+                    Between = segregation::mutual_total(data = db, group = grupo,
+                                                        unit = VAR, weight = peso,
+                                                        se = F)$est[1],
+                    Within =  segregation::mutual_total(data = db, group = grupo,
+                                                        unit = id, weight = peso,
+                                                        se = F, within = VAR)$est[1])
   return(res)
 }
